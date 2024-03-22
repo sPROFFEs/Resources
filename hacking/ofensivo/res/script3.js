42e24fa11fd97968cb8544ad97dcfdf13de0b182
pr0ff3 <150958256+sPROFFEs@users.noreply.github.com>
2024-03-22 16:11:58 +0100

    Updated offensive and added news section

--- hacking/ofensivo/res/script3.js
+++ hacking/ofensivo/res/script3.js
@@ -98,3 +98,13 @@ function handle_keypress(ev) {
 			break
 	}
 }
+
+function w3_open() {
+	document.getElementById("mySidebar").style.display = "block";
+	document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+	document.getElementById("mySidebar").style.display = "none";
+	document.getElementById("myOverlay").style.display = "none";
+}
